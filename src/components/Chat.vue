f05171726a3f3b740a8e1062a19a199ae076c335
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-24 12:19:39 +0100

    initial commit

---
+++ src/components/Chat.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+let message = ref("Hello World");
+let messages = ref([]);
+
+let socket = null;
+
+onMounted(() => {
+  socket = new WebSocket("ws://localhost:3000/primus");
+
+  socket.onmessage = (event) => {
+    let newMessage = JSON.parse(event.data);
+
+    if (newMessage.action === "newMessage") {
+      messages.value.push(newMessage.message);
+    }
+  };
+});
+
+const sendMessage = () => {
+  let newMessage = {
+    message: message.value,
+    action: "newMessage",
+  };
+
+  socket.send(JSON.stringify(newMessage));
+
+  message.value = "";
+};
+</script>
+
+<template>
+  <div>
+    <h1>Chat</h1>
+    <ul>
+      <li v-for="message in messages" :key="message">{{ message }}</li>
+    </ul>
+    <div>
+      <input v-model="message" type="text" />
+      <button @click="sendMessage">Send</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
